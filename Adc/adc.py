04c1d50d3b72ee467a5783a4a6d06344a7da0f98
lallaw8809 <lalbosco55@gmail.com>
2018-01-28 08:21:59 +0000

    Modified the adc code

--- Adc/adc.py
+++ Adc/adc.py
@@ -7,12 +7,21 @@ import time
 import Adafruit_GPIO.SPI as SPI
 import Adafruit_MCP3008
 
+# MCP3008 VDD to Raspberry Pi 3.3V
+# MCP3008 VREF to Raspberry Pi 3.3V
+# MCP3008 AGND to Raspberry Pi GND
+# MCP3008 DGND to Raspberry Pi GND
+# MCP3008 CLK to Raspberry Pi pin 18
+# MCP3008 DOUT(MISO) to Raspberry Pi pin 23
+# MCP3008 DIN(MOSI) to Raspberry Pi pin 24
+# MCP3008 CS/SHDN to Raspberry Pi pin 25
+
 
 # Software SPI configuration:
-#CLK  =  11
-#MISO =   9
-#MOSI =  10
-#CS   =   7
+# CLK  = 23
+# MISO = 19
+# MOSI = 21
+# CS   = 24
 #mcp = Adafruit_MCP3008.MCP3008(clk=CLK, cs=CS, miso=MISO, mosi=MOSI)
 
 # Hardware SPI configuration:
