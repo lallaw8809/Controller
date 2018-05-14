6cdd6b9b860300b2d1138d5cb01c0bcff004347e
lallaw8809 <lalbosco55@gmail.com>
2018-05-14 17:16:46 +0530

    Added a sample code to get/post URL

---
+++ IoT/Linux/post.py
@@ -0,0 +1,22 @@
+#--------------------------------------
+#Program to Post the URL
+#
+#Author : Lal Bosco
+#Date   : 14-May-2018
+#--------------------------------------
+
+#!/usr/bin/env python
+
+import urllib2
+import urllib
+
+query_args = { 'RPI':'Active' }
+
+URL  = "http://www.bpiot.dx.am/health/health.php?t1=4&c=3&t2=2&p=1&s1=This_is_Sting1&s2=String2" 
+
+#Update URL
+data     = urllib.urlencode(query_args)
+request  = urllib2.Request(URL, data)
+response = urllib2.urlopen(request).read()
+
+print 'URL Posted'
