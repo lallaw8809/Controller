568ac18fcdaa731d57e9559ebd98033c183d1481
lallaw8809 <lalbosco55@gmail.com>
2018-05-14 17:20:07 +0530

    Modified code for URL update

--- IoT/Linux/Post.py
+++ IoT/Linux/Post.py
@@ -12,9 +12,11 @@ import urllib
 
 query_args = { 'RPI':'Active' }
 
+URL  = "http://www.bpiot.dx.am/health/health.php?t1=4&c=3&t2=2&p=1&s1=This_is_Sting1&s2=String2" 
+
 #Update URL
-URL = "http://www.bpiot.dx.am/health/health.php?t1=4&c=3&t2=2&p=1&s1=This_is_Sting1&s2=String2"
-data = urllib.urlencode(query_args)
-request = urllib2.Request(URL, data)
+data     = urllib.urlencode(query_args)
+request  = urllib2.Request(URL, data)
 response = urllib2.urlopen(request).read()
+
 print 'URL Posted'
